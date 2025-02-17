3f6b5b2ff896ebe4ce82616378262be111dfc0bf
’Marina <148355147+MaRos10@users.noreply.github.com>
2025-02-17 19:00:43 +0100

    Click on header navigates to setTimer

--- src/components/IntervalHeader/IntervalHeader.jsx
+++ src/components/IntervalHeader/IntervalHeader.jsx
@@ -1,9 +1,23 @@
 import "./IntervalHeader.css";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { TimerContext } from "../TimerContext";
 
 export default function IntervalHeader() {
+  const { stopTimer } = useContext(TimerContext);
+  const navigate = useNavigate();
+
+  const handleHeadertext = () => {
+    setTimeout(() => {
+      stopTimer();
+      navigate("/setTimer");
+    }, 300);
+  };
   return (
     <>
-      <header className="headerText">interval</header>
+      <header className="headerText" onClick={handleHeadertext}>
+        interval
+      </header>
     </>
   );
 }
